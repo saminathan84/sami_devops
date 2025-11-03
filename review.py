6e47a6439cfd27adfcbb08e99e55f73278f66431
saminathan84 <saminathan84@gmail.com>
2025-11-03 13:05:56 +0530

    Delete review.py

--- review.py
+++
@@ -1,8 +0,0 @@
-x=open("sample.hql")
-str= x.readlines()
-if any(item for item in str if "drop" in item):
-	print "Script review failed for sample.hql"
-	exit(1)
-else:
-	print "Script review success for sample.hql"
-	exit(0)
